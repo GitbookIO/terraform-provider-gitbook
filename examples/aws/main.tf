70db520336c29adeda7c652cb134483345dad7b2
David Stotijn <dstotijn@gmail.com>
2023-09-20 16:42:29 +0200

    Improve aws example docs

--- examples/aws/main.tf
+++ examples/aws/main.tf
@@ -8,15 +8,23 @@ terraform {
 
 provider "gitbook" {}
 
+# You can find your organization ID by visiting https://app.gitbook.com opening
+# the settings page of the organization you installed the GitBook Terraform
+# integration for. The organization settings page has a "Copy org ID" button.
+variable "organization_id" {
+  type = string
+}
+
+# In a real-world scenario, you would use actual resources from the `aws`
+# provider, but for the sake of this example, we'll use some fake static data.
 locals {
-  organization_id    = "4Me7JapjYF3sgxrFoKxP"
   account_arn        = "arn:aws:organizations::123456789012:account/o-123456/123456789012"
   my_function_arn    = "arn:aws:lambda:us-east-2:123456789012:function:my-function"
   my_function_region = "us-east-2"
 }
 
 resource "gitbook_entity_schema" "aws_account" {
-  organization_id = local.organization_id
+  organization_id = var.organization_id
   title = {
     singular : "AWS Account"
     plural : "AWS Accounts",
@@ -42,7 +50,7 @@ resource "gitbook_entity_schema" "aws_account" {
 }
 
 resource "gitbook_entity_schema" "aws_lambda_function" {
-  organization_id = local.organization_id
+  organization_id = var.organization_id
   type            = "terraform:aws-lambda-function"
   title = {
     singular = "AWS Lambda Function"
